f96a15125f7b32cd10e695861e1a0f96e91f6400
zetapow <zetapow@gmail.com>
2025-03-01 19:21:04 +1300

    bee info to be added

--- javascript/script.js
+++ javascript/script.js
@@ -5,70 +5,70 @@ const beeSpeciesNZ = [
       commonName: 'European Honey Bee',
       fact: 'Introduced to New Zealand in the 19th century, they are crucial for pollinating many crops.',
       pun: 'Why did the honey bee go to school? To get a little buzz-ter education!',
-      src: './images/beeArray/european-honey-bee.jpg',
+      src: './images/beeArray/bee01.jpg',
    },
    {
       scientificName: 'Leioproctus fulvescens',
       commonName: 'Native Yellow-Faced Bee',
       fact: 'One of the few native bee species in New Zealand, they are solitary bees.',
       pun: 'What do you call a bee that can’t make up its mind? A maybe!',
-      src: './images/beeArray/yellow-faced-bee.jpg',
+      src: './images/beeArray/bee02.jpg',
    },
    {
       scientificName: 'Lasioglossum sordidum',
       commonName: 'Native Sweat Bee',
       fact: 'These bees are known for their metallic sheen and are important pollinators in native ecosystems.',
       pun: 'Why do sweat bees make great comedians? They always work the crowd!',
-      src: './images/beeArray/sweat-bee.jpg',
+      src: './images/beeArray/bee03.jpg',
    },
    {
       scientificName: 'Hylaeus spp.',
       commonName: 'Native Masked Bee',
       fact: 'Masked bees are small and often nest in hollow stems or wood cavities.',
       pun: 'Why did the masked bee get invited to all the parties? Because it always brought the buzz!',
-      src: './images/beeArray/masked-bee.jpg',
+      src: './images/beeArray/bee04.jpg',
    },
    {
       scientificName: 'Bombus terrestris',
       commonName: 'Buff-Tailed Bumblebee',
       fact: 'Introduced to New Zealand for pollination purposes, they are now widespread and effective pollinators.',
       pun: 'What do you call a bumblebee with a great sense of direction? A bee-line!',
-      src: './images/beeArray/buff-tailed-bumblebee.jpg',
+      src: './images/beeArray/bee05.jpg',
    },
    {
       scientificName: 'Colletes spp.',
       commonName: 'Plasterer Bee',
       fact: 'Plasterer bees line their nests with a waterproof secretion, giving them their name.',
       pun: 'Why did the plasterer bee get a promotion? Because it always stuck to the job!',
-      src: './images/beeArray/plasterer-bee.jpg',
+      src: './images/beeArray/bee06.jpg',
    },
    {
       scientificName: 'Euryglossina spp.',
       commonName: 'Dwarf Bee',
       fact: 'These tiny bees are often overlooked but play a vital role in pollinating small native flowers.',
       pun: 'Why did the dwarf bee start a band? Because it had the perfect buzz for music!',
-      src: './images/beeArray/dwarf-bee.jpg',
+      src: './images/beeArray/bee07.jpg',
    },
    {
       scientificName: 'Nomia spp.',
       commonName: 'Metallic Sweat Bee',
       fact: 'Known for their iridescent colors, these bees are attracted to human sweat for its salt content.',
       pun: 'Why did the metallic sweat bee win an award? Because it was outstanding in its field!',
-      src: './images/beeArray/metallic-sweat-bee.jpg',
+      src: './images/beeArray/bee08.jpg',
    },
    {
       scientificName: 'Megachile spp.',
       commonName: 'Leafcutter Bee',
       fact: 'Leafcutter bees cut neat circles from leaves to build their nests.',
       pun: 'Why did the leafcutter bee become a tailor? Because it was great at cutting patterns!',
-      src: './images/beeArray/leafcutter.webp',
+      src: './images/beeArray/bee09.webp',
    },
    {
       scientificName: 'Xylocopa spp.',
       commonName: 'Carpenter Bee',
       fact: 'Carpenter bees bore into wood to create nests, but they are important pollinators for many plants.',
       pun: 'Why did the carpenter bee get kicked out of the bar? It kept drilling into the furniture!',
-      src: './images/beeArray/carpenter.webp',
+      src: './images/beeArray/bee10.webp',
    },
 ];
 
@@ -269,6 +269,9 @@ leftArrow.addEventListener('click', prevImage);
 thumbnails.forEach((thumbnail, index) => {
    thumbnail.addEventListener('click', () => selectThumbnail(index));
 });
+
+/*
 // 3 second delay
 const ADVANCE_DELAY = 3000;
 setInterval(nextImage, ADVANCE_DELAY);
+*/
